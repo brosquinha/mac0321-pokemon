8894e62d61c7be0a20d4b8ef7b8bc7ae811804c2
mac0321 41 <mac0321.41@metralha.cec.ime.usp.br>
2017-05-05 09:55:28 -0300

    Adicionando Versus

---
+++ src/pokemon/Versus.java
@@ -0,0 +1,34 @@
+package c07.controller;
+
+public class Versus extends Controller {
+	private Pokemon[] pokemons1;
+	private Pokemon[] pokemons2;
+	private Trainer trainer1;
+	private Trainer trainer2;
+	
+	private class Attack extends Event {
+		public Attack(long eventTime) {
+			super(eventTime);
+		}
+		public void action() {
+			
+		}
+		public String description() {
+			return "Pokemon ataca";
+		}
+	}
+	
+	private class ChangePokemon extends Event {
+		public ChangePokemon(long eventTime) {
+			super(eventTime);
+		}
+		public void action() {
+			// TODO Auto-generated method stub
+			
+		}
+		public String description() {
+			return "Trocando pokemon";
+		}
+	}
+	
+}
