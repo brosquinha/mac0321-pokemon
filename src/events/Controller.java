5add6b4a5fafe02936b2a3e8efcefd69f69b93d5
anor4k <noel.eliezer@gmail.com>
2017-05-12 10:53:44 -0300

    updated controller

--- src/events/Controller.java
+++ src/events/Controller.java
@@ -31,7 +31,11 @@ class EventSet {
 
 public class Controller {
 	private EventSet es1 = new EventSet(), es2 = new EventSet();
-	public void addEvent(Event c) { es1.add(c); }
+	public void addEvent(int treinador, Event c) { 
+		if (treinador == 1) es1.add(c);
+		else if (treinador == 2 ) es2.add(c);
+		else return;
+	}
 	public void run() {
 		Event e1, e2;
 		while(((e1 = es1.getNext()) != null) && ((e2 = es2.getNext()) != null) ) {
@@ -43,6 +47,14 @@ public class Controller {
 				e2.action();
 				System.out.println(e2.description());
 				es2.removeCurrent();
+			} else {
+				e2.action();
+				System.out.println(e2.description());
+				es2.removeCurrent();
+				if (es2.getNext() == null) break; // a rodada pode terminar no meio do loop
+				e1.action();
+				System.out.println(e1.description());
+				es1.removeCurrent();
 			}
 		}
 	}
