eed0c5d0b24bd1f8b88cf09542015b06fe3cccaa
mac0321 41 <mac0321.41@metralha.cec.ime.usp.br>
2017-05-12 11:19:54 -0300

    Funcionando melhor

--- src/pokemon/Versus.java
+++ src/pokemon/Versus.java
@@ -2,7 +2,7 @@ package pokemon;
 import events.*;
 
 public class Versus extends Controller {
-	private Trainer[] trainers;
+	private Trainer[] trainers = new Trainer[2];
 	
 	private class Attack extends Event {
 		private Pokemon attacker;
@@ -23,8 +23,10 @@ public class Versus extends Controller {
 			this.defenderTrainer = (this.attackerTrainer == 0) ? 1 : 0;
 		}
 		public void action() {
-			if (!attacker.isDead())
+			if (!attacker.isDead()) {
 				this.attacker.attack(this.attack, this.defender);
+				addEvent(this.attackerTrainer+1, new Batalhar(this.attackerTrainer + 1));
+			}
 			else {
 				ChangePokemon p = new ChangePokemon(trainers[this.defenderTrainer]);
 				p.action();
@@ -48,6 +50,7 @@ public class Versus extends Controller {
 				pokemonAtivo++;
 			}
 			treinador.setPokemonAtivo(pokemonAtivo);
+			addEvent(treinador.id, new Batalhar(treinador.id));
 		}
 		public String description() {
 			return "Trocando pokemon";
@@ -57,13 +60,14 @@ public class Versus extends Controller {
 	private class UseItem extends Event {
 		private Trainer treinador;
 		private int item;
-		public UseItem(int prioridade, Trainer treinador, int item) {
-			super(prioridade);
+		public UseItem(Trainer treinador, int item) {
+			super(200);
 			this.treinador = treinador;
 			this.item = item;
 		}
 		public void action() {
 			treinador.useItem(item);
+			addEvent(treinador.id, new Batalhar(treinador.id));
 		}
 		public String description() {
 			return "Usando item de cura";
@@ -72,8 +76,8 @@ public class Versus extends Controller {
 	
 	private class Run extends Event {
 		private Trainer treinador;
-		public Run(int prioridade, Trainer treinador) {
-			super(prioridade);
+		public Run(Trainer treinador) {
+			super(500);
 			this.treinador = treinador;
 		}
 		public void action() {
@@ -87,16 +91,34 @@ public class Versus extends Controller {
 	private class Batalhar extends Event {
 		private Trainer trainer1;
 		private Trainer trainer2;
+		private int idTrainer;
 		
-		public Batalhar(Trainer trainer1, Trainer trainer2) {
-			super(0);
-			this.trainer1 = trainer1;
-			this.trainer2 = trainer2;
+		public Batalhar(int idTrainer) {
+			super(3);
+			this.idTrainer = idTrainer;
+			this.trainer1 = trainers[idTrainer-1];
+			this.trainer2 = (idTrainer == 1) ? trainers[1] : trainers[0];
 		}
 		public void action() {
 			double n = Math.random();
-			addEvent(new Attack(this.trainer1.pokemons[this.trainer1.pokemonAtivo], 
-					2, this.trainer2.pokemons[this.trainer2.pokemonAtivo], 1));
+			System.out.println("Iniciando rodada");
+			if (n > 0.5)
+			{
+				addEvent(this.idTrainer, new Attack(this.trainer1.pokemons[this.trainer1.pokemonAtivo], 
+						2, this.trainer2.pokemons[this.trainer2.pokemonAtivo], 1));
+			}
+			else if (n > 0.3)
+			{
+				addEvent(this.idTrainer, new UseItem(trainer1, 2));
+			}
+			else if (n > 0.1)
+			{
+				addEvent(this.idTrainer, new ChangePokemon(trainer1));
+			}
+			else
+			{
+				addEvent(this.idTrainer, new Run(trainer1));
+			}
 			// Instead of hard-wiring, you could parse
 			// configuration information from a text
 			// file here:
@@ -120,9 +142,14 @@ public class Versus extends Controller {
 		Versus vs = new Versus();
 		Pokemon[] pokemons1 = {
 			new Pokemon("Blastoise", 300),
+			new Pokemon("Pikachu", 300),
+			new Pokemon("Charizard", 300),
+			new Pokemon("Mewtwo", 300),
 		};
 		vs.trainers[0] = new Trainer(1, pokemons1);
 		vs.trainers[1] = new Trainer(2, pokemons1);
-		vs.addEvent(vs.new Batalhar(vs.trainers[0], vs.trainers[1]));
+		vs.addEvent(1, vs.new Batalhar(1));
+		vs.addEvent(2, vs.new Batalhar(2));
+		vs.run();
 	}
 }
